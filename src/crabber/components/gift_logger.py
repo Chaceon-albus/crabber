dd09a953d6cfccd686ea50b495df452cf5f451c7
capric98 <42015599+capric98@users.noreply.github.com>
2026-03-24 23:49:31 -0500

    rename variables

--- src/crabber/components/gift_logger.py
+++ src/crabber/components/gift_logger.py
@@ -11,6 +11,7 @@ def get_handler(*args, **kwargs) -> Callable[[dict], Awaitable[None]]:
     async def handler(event: dict) -> None:
         cmd = event.get("data", {}).get("cmd", "unknown")
         data = event.get("data", {}).get("data", {})
+        value_in_cny = 0.0
 
         match cmd:
             case "SEND_GIFT":
@@ -23,8 +24,9 @@ def get_handler(*args, **kwargs) -> Callable[[dict], Awaitable[None]]:
                 price = data.get("price", 0)
 
                 coin = data.get("total_coin", price * num)
+                value_in_cny = coin_to_cny(coin)
 
-                logger.info(f"{user} {action}了 {gift_name}×{num}，价值￥{coin_to_cny(coin):.2f}")
+                logger.info(f"{user} {action}了 {gift_name}×{num}，价值￥{value_in_cny:.2f}")
 
             case "USER_TOAST_MSG":
                 num  = data.get("num", 1)
@@ -32,16 +34,20 @@ def get_handler(*args, **kwargs) -> Callable[[dict], Awaitable[None]]:
                 role = data.get("role_name", "舰长")
                 user = data.get("username", "[unknown]")
                 price = data.get("price", 0) # this is total price, not unit price
-                logger.info(f"{user} 开通了{num}个{unit}的{role}，价值￥{coin_to_cny(price):.2f}")
+                value_in_cny = coin_to_cny(price)
+                logger.info(f"{user} 开通了{num}个{unit}的{role}，价值￥{value_in_cny:.2f}")
 
             case "SUPER_CHAT_MESSAGE":
                 user = data.get("user_info", {}).get("uname", "[unknown]")
-                price = data.get("price", 0) # CNY price
                 message = data.get("message", "")
-                logger.info(f"{user} 发送了价值￥{coin_to_cny(price):.2f}的醒目留言: {message}")
+                value_in_cny = data.get("price", 0) # CNY price
+                logger.info(f"{user} 发送了￥{value_in_cny:.2f}的醒目留言: {message}")
 
             case _:
                 logger.warning(f"received unsupported event: {cmd}")
+
+
+        # value_in_cny...
 
 
     return handler
