f75a43aea7da4c8a36ac48765d3a5c1d64065396
capric98 <42015599+capric98@users.noreply.github.com>
2026-05-07 18:31:00 -0500

    use empty_handler

--- src/crabber/components/notify/notify.py
+++ src/crabber/components/notify/notify.py
@@ -4,6 +4,7 @@ from typing import Callable, Awaitable
 from crabber.crabber import Crabber
 from crabber.room_info import RoomInfo
 
+from crabber.components.empty_handler import empty_handler
 from crabber.components.notify.ntfy_provider import send_notify as send_ntfy_notify
 from crabber.components.notify.napcat_provider import send_notify as send_napcat_notify
 
@@ -15,9 +16,6 @@ def get_handler(ctx: Crabber, channels: list[dict], *args, **kwargs) -> Callable
 
     logger = ctx.logger
 
-    async def handler(event: dict) -> None:
-        # no handler needed for notify component
-        pass
 
     async def _on_room_online(room_info: RoomInfo, misfire_grace_time: float = 60.0) -> None:
         if room_info.start_time is not None:
@@ -44,4 +42,4 @@ def get_handler(ctx: Crabber, channels: list[dict], *args, **kwargs) -> Callable
     ctx.add_online_callback(_on_room_online)
 
 
-    return handler
+    return empty_handler
